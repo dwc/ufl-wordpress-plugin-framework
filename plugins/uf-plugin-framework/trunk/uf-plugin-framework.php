26bd4319835a954cf614f1fa0bbf4dac7bc02d67
dwc <dwc@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2006-05-31 22:45:09 +0000

    Add check for authentication for wp-admin requests

--- plugins/uf-plugin-framework/trunk/uf-plugin-framework.php
+++ plugins/uf-plugin-framework/trunk/uf-plugin-framework.php
@@ -26,9 +26,23 @@ function uf_plugin_framework_init() {
 	$uf_plugin_framework_action = get_query_var('uf_plugin_framework_action');
 
 	if ($uf_plugin_framework_plugin and $uf_plugin_framework_action) {
+		if (uf_plugin_framework_is_admin_request()) {
+			if (function_exists('auth_redirect')) {
+				auth_redirect();
+			}
+			else {
+				exit('UF Plugin Framework: Unable to redirect for authentication');
+			}
+		}
+
 		$the_action = UfUtilities::get_action_name($uf_plugin_framework_plugin, $uf_plugin_framework_action);
 		do_action($the_action);
+		exit('UF Plugin Framework: Finished');
 	}
+}
+
+function uf_plugin_framework_is_admin_request() {
+	return (strstr($_SERVER['PHP_SELF'], 'wp-admin'));
 }
 
 function uf_plugin_framework_uri($plugin, $action, $is_admin = false) {
