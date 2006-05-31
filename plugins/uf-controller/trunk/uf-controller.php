7680ea853bc009b91682f2d29357863aa088cff6
dwc <dwc@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2006-05-31 22:31:18 +0000

    Stubs for controller functionality

--- plugins/uf-controller/trunk/uf-controller.php
+++ plugins/uf-controller/trunk/uf-controller.php
@@ -12,8 +12,30 @@ define('UF_CONTROLLER_PLUGIN_BASE', dirname(__FILE__) . '/');
 
 /*
  * Required files for the controller framework; other plugins can include stuff
- * from lib as appropriate
+ * from lib as appropriate.
  */
 require_once('lib/class.UfAdminController.php');
 require_once('lib/class.UfController.php');
+
+/*
+ * Bootstrap the controller framework.
+ */
+add_action('init', 'uf_controller_init');
+
+function uf_controller_init() {
+	$uf_controller_plugin = get_query_var('uf_controller_plugin');
+	$uf_controller_action = get_query_var('uf_controller_action');
+
+	if ($uf_controller_plugin and $uf_controller_action) {
+		$the_action = UfUtilities::get_action_name($uf_controller_plugin, $uf_controller_action);
+		do_action($the_action);
+	}
+}
+
+function uf_controller_uri($plugin, $action, $is_admin = false) {
+}
+
+function uf_admin_controller_uri($plugin, $action) {
+	return uf_controller_uri($plugin, $action, true);
+}
 ?>
