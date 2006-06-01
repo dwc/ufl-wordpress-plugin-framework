3d51bfed9175e2c0d21b1b5df3ebe525ede8f32f
dwc <dwc@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2006-06-01 01:47:03 +0000

    Spacing

--- plugins/uf-plugin-framework/trunk/uf-plugin-framework.php
+++ plugins/uf-plugin-framework/trunk/uf-plugin-framework.php
@@ -22,6 +22,7 @@ require_once(UF_PLUGIN_FRAMEWORK_LIBRARY . '/class.UfUtilities.php');
  */
 add_action('init', 'uf_plugin_framework_init');
 
+
 function uf_plugin_framework_init() {
 	$uf_plugin_framework_plugin = $_REQUEST['uf_plugin_framework_plugin'];
 	$uf_plugin_framework_action = $_REQUEST['uf_plugin_framework_action'];
@@ -54,10 +55,7 @@ function uf_plugin_framework_is_admin_request() {
 	return (strstr($_SERVER['PHP_SELF'], 'wp-admin'));
 }
 
-function uf_plugin_framework_uri($plugin, $action, $is_admin = false) {
-}
-
-function uf_admin_plugin_framework_uri($plugin, $action) {
-	return uf_plugin_framework_uri($plugin, $action, true);
+function uf_plugin_framework_uri() {
+	bloginfo('url');
 }
 ?>
