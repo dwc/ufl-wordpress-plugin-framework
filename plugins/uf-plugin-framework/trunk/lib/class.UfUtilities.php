e3dcc03837daef8218b8868421409b8b8b96cd45
dwc <dwc@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2008-10-23 18:22:25 +0000

    Refactor method to take attachment data and a local filename to inject it into WordPress

--- plugins/uf-plugin-framework/trunk/lib/class.UfUtilities.php
+++ plugins/uf-plugin-framework/trunk/lib/class.UfUtilities.php
@@ -46,6 +46,49 @@ if (! class_exists('UfUtilities')) {
 
 			return "uf__{$basename}__{$action}";
 		}
+
+		/*
+		 * Inject an attachment into WordPress, by copying an
+		 * existing file and simulating all the work that
+		 * WordPress does (in wp_handle_upload) on file upload
+		 * but doesn't have refactored.
+		 */
+		function inject_attachment($attachment, $file) {
+			$uploads = wp_upload_dir($attachment['post_date']);
+			if ($uploads['error']) die("Couldn't get upload directory.");
+			//echo "uploads = "; print_r($uploads); ob_flush(); flush();
+
+			$filename = wp_unique_filename($uploads['path'], basename($file));
+			$new_file = $uploads['path'] . '/' . $filename;
+			if (! copy($file, $new_file)) die("Couldn't copy file $file to $new_file.");
+			//echo "copied file = [$file] to new_file = [$new_file]\n"; ob_flush(); flush();
+
+			$stat = stat(dirname($new_file));
+			$perms = $stat['mode'] & 0000666;
+			chmod($new_file, $perms);
+			//echo "chmod'd new_file = [$new_file] to perms = [$perms]\n"; ob_flush(); flush();
+
+			$url = $uploads['url'] . '/' . $filename;
+			//echo "url = [$url]\n"; ob_flush(); flush();
+
+			$size = getimagesize($new_file);
+			//echo "size = "; print_r($size); ob_flush(); flush();
+			apply_filters('wp_handle_upload', array('file' => $new_file, 'url' => $url, 'type' => $size['mime']));
+			//echo "applied wp_handle_upload filters\n"; ob_flush(); flush();
+
+			// Now, add the attachment
+			$attachment['guid'] = $url;
+			$attachment['post_mime_type'] = $size['mime'];
+
+			$attachment_id = wp_insert_attachment($attachment, $new_file);
+			//echo "inserted attachment_id = [$attachment_id]\n"; ob_flush(); flush();
+			if (! is_wp_error($attachment_id)) {
+				wp_update_attachment_metadata($attachment_id, wp_generate_attachment_metadata($attachment_id, $new_file));
+				//echo "generated metadata for attachment_id = [$attachment_id]\n"; ob_flush(); flush();
+			}
+
+			return $attachment_id;
+		}
 	}
 }
 ?>
