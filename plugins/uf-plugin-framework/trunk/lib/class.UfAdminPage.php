e5fd41788d769bdf6444027b6ec57d4df5b810c1
dwc <dwc@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2009-03-11 22:56:18 +0000

    Refactor display to add default wrapper and add common submit button display code

--- plugins/uf-plugin-framework/trunk/lib/class.UfAdminPage.php
+++ plugins/uf-plugin-framework/trunk/lib/class.UfAdminPage.php
@@ -20,6 +20,36 @@ if (! class_exists('UfAdminPage')) {
 		}
 
 		function display() {
+			$this->display_start();
+			$this->display_body();
+			$this->display_end();
+		}
+
+		function display_start() {
+?>
+<div class="wrap">
+	<h2><?php echo htmlspecialchars($this->title); ?></h2>
+<?php if ($this->description): ?>
+	<p><?php echo $this->description; ?></p>
+<?php endif; ?>
+<?php
+		}
+
+		function display_end() {
+?>
+</div><!-- .wrap -->
+<?php
+		}
+
+		function display_body() {
+		}
+
+		function submit_button($label, $primary = true, $level_one_padding = "\t\t", $level_two_padding = "\t\t\t") {
+?>
+<?php echo $level_one_padding; ?><p class="submit">
+<?php echo $level_two_padding; ?><input type="submit" value="<?php echo htmlspecialchars($label); ?>"<?php if ($primary): ?> class="button-primary"<?php endif; ?> />
+<?php echo $level_one_padding; ?></p><!-- .submit -->
+<?php
 		}
 	}
 }
--- plugins/uf-plugin-framework/trunk/lib/class.UfOptionsPage.php
+++ plugins/uf-plugin-framework/trunk/lib/class.UfOptionsPage.php
@@ -15,71 +15,48 @@ if (! class_exists('UfOptionsPage')) {
 			$this->{get_parent_class(__CLASS__)}($title, $description, 'manage_options', 'options');
 		}
 
-		function display() {
-			$this->display_start();
-			$this->display_options();
-			$this->display_end();
-		}
-
-		function display_start() {
-?>
-<div class="wrap">
-  <h2><?php echo htmlspecialchars($this->title); ?></h2>
-<?php if ($this->description): ?>
-  <p><?php echo $this->description; ?></p>
-<?php endif; ?>
-<?php
-		}
-
-		function display_end() {
-?>
-</div>
-<?php
-		}
-
-		function display_options() {
+		function display_body() {
 			$page_options = array();
 ?>
-  <form method="post" action="options.php">
+	<form method="post" action="options.php">
 <?php foreach ($this->option_groups as $option_group): ?>
-    <h3><?php echo htmlspecialchars($option_group->name); ?></h3>
-    <table class="form-table">
+		<h3><?php echo htmlspecialchars($option_group->name); ?></h3>
+		<table class="form-table">
 <?php     foreach ($option_group->options as $option): ?>
 <?php         $page_options[] = $option->name; ?>
 <?php         $this->display_option($option); ?>
 <?php     endforeach; ?>
-    </table>
+		</table><!-- .form-table -->
 <?php endforeach; ?>
 
-    <?php if (function_exists('wp_nonce_field')): wp_nonce_field('update-options'); endif; ?>
+		<?php if (function_exists('wp_nonce_field')): wp_nonce_field('update-options'); endif; ?>
 
-    <input type="hidden" name="action" value="update" />
-    <input type="hidden" name="page_options" value="<?php echo implode(",", $page_options); ?>" />
-    <p class="submit">
-      <input type="submit" name="Submit" value="Update Options" class="button-primary" />
-    </p>
-  </form>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="<?php echo implode(",", $page_options); ?>" />
+
+<?php $this->submit_button('Update Options'); ?>
+	</form>
 <?php
 		}
 
 		function display_option($option) {
 ?>
-      <tr valign="top">
-        <th scope="row"><label for="<?php echo htmlspecialchars($option->name); ?>"><?php echo htmlspecialchars($option->description); ?></label></th>
-        <td>
+			<tr valign="top">
+				<th scope="row"><label for="<?php echo htmlspecialchars($option->name); ?>"><?php echo htmlspecialchars($option->description); ?></label></th>
+				<td>
 <?php         $option_value = get_option($option->name); ?>
 <?php         if (strpos($option_value, "\n") === false): ?>
-          <input type="text" name="<?php echo htmlspecialchars($option->name); ?>" id="<?php echo htmlspecialchars($option->name); ?>" value="<?php echo htmlspecialchars($option_value); ?>" size="40" /><?php echo htmlspecialchars($option->units ? ' ' . $option->units : ''); ?>
+					<input type="text" name="<?php echo htmlspecialchars($option->name); ?>" id="<?php echo htmlspecialchars($option->name); ?>" value="<?php echo htmlspecialchars($option_value); ?>" size="40" /><?php echo htmlspecialchars($option->units ? ' ' . $option->units : ''); ?>
 <?php         else: ?>
-          <textarea name="<?php echo htmlspecialchars($option->name); ?>" id="<?php echo htmlspecialchars($option->name); ?>" rows="10" cols="40"><?php echo htmlspecialchars($option_value); ?></textarea>
+					<textarea name="<?php echo htmlspecialchars($option->name); ?>" id="<?php echo htmlspecialchars($option->name); ?>" rows="10" cols="40"><?php echo htmlspecialchars($option_value); ?></textarea>
 <?php         endif; ?>
 <?php         if ($option->default_value): ?>
 
-          <br />
-          Default is <code><?php echo htmlspecialchars($option->default_value); ?></code>
+					<br />
+					Default is <code><?php echo htmlspecialchars($option->default_value); ?></code>
 <?php         endif; ?>
-        </td>
-      </tr>
+				</td>
+			</tr>
 <?php
 		}
 	}
