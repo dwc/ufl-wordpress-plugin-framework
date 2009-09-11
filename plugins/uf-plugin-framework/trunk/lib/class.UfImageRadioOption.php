f481ae26ba394f43b9beb05dc22272a6e42f8d56
spooner <spooner@2a694338-c2fd-11de-b13d-e5b83f1b426b>
2009-09-11 19:19:44 +0000

    Adding in radio button with image

---
+++ plugins/uf-plugin-framework/trunk/lib/class.UfImageRadioOption.php
@@ -0,0 +1,30 @@
+<?php
+require_once('class.UfRadioOption.php');
+
+
+/*
+ * An radio button with an associated image for a UF WordPress plugin.
+ */
+if (! class_exists('UfImageRadioOption')) {
+	class UfImageRadioOption extends UfRadioOption {
+
+		function display_label() {
+?>
+<?php
+		}
+
+		function display_value_body($value) {
+?>
+
+		  <input type="radio" name="<?php echo htmlspecialchars($this->name); ?>" id="<?php echo htmlspecialchars($this->default_value); ?>" value="<?php echo htmlspecialchars($this->default_value) ?>"<?php if (strcmp($value, $this->default_value) == 0): ?> checked <?php endif; ?> /> <label for="<?php echo htmlspecialchars($this->default_value); ?>"><?php echo htmlspecialchars($this->description); ?> <br />
+				<img src="<?php echo htmlspecialchars($this->units); ?>" /></label>
+<?php
+		}
+
+		function display_value_default() {
+?>
+<?php
+		}
+	}
+}
+?>
---
+++ plugins/uf-plugin-framework/trunk/lib/class.UfRadioOption.php
@@ -0,0 +1,12 @@
+<?php
+require_once('class.UfOption.php');
+
+
+/*
+ * An boolean (i.e. checkbox) option for a UF WordPress plugin.
+ */
+if (! class_exists('UfRadioOption')) {
+	class UfRadioOption extends UfOption {
+	}
+}
+?>
